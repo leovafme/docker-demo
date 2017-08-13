93e535cc1bc077dd969325cdf68ff4bf62e510ae
Leonardo A <leovafme@hotmail.com>
2017-08-13 14:51:57 -0500

    commit initial
    firts commit to docker project

---
+++ php/www/index.php
@@ -0,0 +1,9 @@
+<?php
+echo 'Docker corriendo en otro contendor!';
+echo '<br>';
+echo 2 + 4;
+echo '<br>';
+echo '2'.'4';
+echo '<br>';
+echo 'otro salto de linea, esto tiene contenedores<br>';
+echo 'Versión actual de PHP: ' . phpversion();
